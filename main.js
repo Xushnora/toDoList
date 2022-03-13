92283c6f2ee5fa4106ecb8849f0230532b7c9915
Xushnora <xushnoranishonova@gmail.com>
2022-03-13 06:59:49 -0700

    changed

--- main.js
+++ main.js
@@ -29,13 +29,26 @@ function addItem(e) {
         let newBox = document.createElement("div");
         newBox.className = "buttons-div";
     
-        let deleteBtn = document.createElement("button");
-        deleteBtn.className = "close-btn";
-        deleteBtn.innerHTML = "+";
-    
         let pushBtn = document.createElement("button");
         pushBtn.className = "close";
         pushBtn.innerHTML = "-";
+
+        let deleteBtn = document.createElement("button");
+        deleteBtn.className = "close-btn";
+        deleteBtn.innerHTML = "+";
+
+        pushBtn.addEventListener('click', removeBtn);
+
+        function removeBtn() {
+            li.remove();
+        }
+
+        deleteBtn.addEventListener('click', overlineBtn);
+
+        function overlineBtn() {
+            li.style.textDecoration = "line-through";
+            li.style.color = "green";3
+        }
     
         console.log(li);
     
